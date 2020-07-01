0110525b199fae3ff4efb994d60ed42ccac7ed8d
Jimin Huh <jiminhuh95@gmail.com>
2020-06-30 23:21:58 -0500

    variables, logical operators, control flow and vectors.

---
+++ derek/derek.cpp
@@ -0,0 +1,53 @@
+//
+// Created by Jimin Huh on 6/30/20.
+//
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <limits>
+#include <vector>
+#include <numeric>
+#include <sstream>
+#include <ctime>
+#include <cmath>
+
+using namespace std;
+
+//int imGlobal = 0;
+//const double PI = 3.141;
+int main () {
+//    cout << "Hello World." << endl;
+//    printf("Sum = %.5f\n", PI);
+//    printf("%c %d %5d %.3f %s\n", 'A', 5, 51201, 0.16125127, "Hello!");
+//    bool married = false;
+//    char myGrade = 'A';
+
+//    string q1 = "Enter a Number : ";
+//    string num1, num2;
+//    cout << q1;
+//    cin >> num1;
+//    cout << "Enter another number : ";
+//    cin >> num2;
+//    int nNum1 = stoi(num1);
+//    int nNum2 = stoi(num2);
+//    printf("%d + %d = %d\n", nNum1, nNum2, (nNum1 + nNum2));
+
+//    string sAge;
+//    cout << "Enter your Birthday:  ";
+//    cin >> sAge;
+//    int nAge = stoi(sAge);
+//
+//    if ((nAge >= 1) && (nAge <= 18)) {
+//        cout << "Important birthday!" << endl;
+//    } else {
+//        cout << "You are too old to celebrate your birthday!" << endl;
+//    }
+
+//    vectors <int> vNums(2);
+//    vNums[0] = 1;
+//    vNums[1] = 2;
+//    vNums.push_back(3);
+
+    return 0;
+}
